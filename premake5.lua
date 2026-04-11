ff5276f516b9c0c5581094125139fbcd1332134a
LongJia Lin <jia1191878105@gmail.com>
2026-04-10 19:44:55 -0700

    Revert "Update premake5.lua"
    This reverts commit 0726691b3876bd688a9721a1f34fd2b479ef047a.

--- premake5.lua
+++ premake5.lua
@@ -18,10 +18,6 @@ files
     "imstb_truetype.h",
     "imgui_tables.cpp",
     "imgui_demo.cpp",
-    "backends/imgui_impl_opengl3.cpp",
-    "backends/imgui_impl_opengl3.h",
-    "backends/imgui_impl_opengl3_loader.h"
-
 }
 
 filter "system:window"
