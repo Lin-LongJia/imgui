f2d3f14f30d28efe75dca5179c95578809907ea9
LongJia Lin <jia1191878105@gmail.com>
2026-04-11 20:27:15 -0700

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "ImGui"
+kind "StaticLib"
+language "C++"
+
+targetdir("bin/" .. outputdir .. "/%{prj.name}")
+objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+files
+{
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_draw.cpp",
+    "imgui_internal.h",
+    "imgui_widgets.cpp",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
+    "imgui_tables.cpp",
+    "imgui_demo.cpp",
+}
+
+filter "system:window"
+systemversion "latest"
+cppdialect "C++17"
+staticruntime "On"
+
+filter { "system:window", "configurations:Debug" }
+buildoptions "/MDd"
+
+filter { "system:window", "configurations:Release" }
+buildoptions "/MT"
+
+filter { "system:window", "configurations:Dist" }
+buildoptions "/MD"
